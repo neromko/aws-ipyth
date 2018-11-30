5f56f5db4b7089f161bddd2833483b59707183cf
Paweł Sulik <sulikpawel@gmail.com>
2018-11-30 16:10:42 +0100

    first=4

--- test/shotty.py
+++ test/shotty.py
@@ -1,7 +1,22 @@
 import boto3
-if __name__ == '__main__':
-     session = boto3.Session(profile_name='pawels_linxsys_testing', region_name='us-east-1')
-     ec2 = session.resource('ec2')
+import click
 
-     for i in ec2.instances.all():
-        print(i)
+
+session = boto3.Session(profile_name='pawels_linxsys_testing', region_name='us-east-1')
+ec2 = session.resource('ec2')
+
+@click.command()
+
+def list_instances():
+    "Lista"
+    for i in ec2.instances.all():
+       print('. '.join((
+       i.id,
+       i.instance_type,
+       i.state['Name']
+       )))
+    return
+
+if __name__ == '__main__':
+
+    list_instances()
