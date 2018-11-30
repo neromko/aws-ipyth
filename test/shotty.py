b78a3567f22af9bcc49c999af40bd7e9eb401951
Paweł Sulik <sulikpawel@gmail.com>
2018-11-30 14:47:46 +0100

    first

---
+++ test/shotty.py
@@ -0,0 +1,7 @@
+import boto3
+if __name__ == '__main__':
+     session = boto3.Session(profile_name='pawels_linxsys_testing', region_name='us-east-1')
+     ec2 = session.resource('ec2')
+
+     for i in ec2.instances.all():
+        print(i)
